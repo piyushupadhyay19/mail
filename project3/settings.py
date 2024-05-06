0adc64d7199007781e7bd1d845dce873088750b3
Piyush Upadhyay <upadhyaykpiyush@gmail.com>
2024-05-06 16:01:03 +0530

    replace django_heroku lib with django_on_heroku

--- project3/settings.py
+++ project3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+import django_on_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,4 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mail/static'),)
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
